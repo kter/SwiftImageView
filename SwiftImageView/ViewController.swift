c6e9bab1173586acd2c2c9be88cf158c5a3c3064
kter <me@kter.jp>
2017-10-15 21:38:25 +0900

    fix

--- SwiftImageView/ViewController.swift
+++ SwiftImageView/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var changeFlag = false
+    @IBOutlet weak var TopImage: UIImageView!
+    
+    @IBAction func ChangeImage(_ sender: Any) {
+        if (changeFlag == true) {
+            TopImage.image = UIImage(named: "back1.jpg")
+            changeFlag = false
+        } else{
+            TopImage.image = UIImage(named: "back2.jpg")
+            changeFlag = true
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TopImage.image = UIImage(named: "back1.jpg")
     }
 
     override func didReceiveMemoryWarning() {
